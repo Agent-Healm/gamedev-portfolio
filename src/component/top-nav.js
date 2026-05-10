dbab1d31656b2a33f863dac4a804e3898e02dc9d
Healm <116085297+Agent-Healm@users.noreply.github.com>
2026-05-10 20:49:46 +0700

    added top navbar component

---
+++ src/component/top-nav.js
@@ -0,0 +1,11 @@
+function topNav(){
+        return `
+        <nav class="mb-20">
+                <h1 class="font-extrabold w-fit">
+                        KEVIN MAVERICK - GAME DEVELOPER PORTFOLIO
+                </h1>
+        </nav>
+        `;
+}
+
+export default topNav; 
